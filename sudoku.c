9b3f67b3f11ab9b6e76af89dfa67e09bd2c42e70
Your Name <you@example.com>
2024-05-16 02:35:48 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -86,7 +86,6 @@ int is_valid(Node* n){
     return 1;
 }
 
-
 List* get_adj_nodes(Node* n){
     List* list=createList();
     int i,j;
@@ -125,7 +124,7 @@ Node* DFS(Node* initial, int* cont){
 
 
 
-/*
+
 int main( int argc, char *argv[] ){
 
   Node* initial= read_file("s12a.txt");;
@@ -136,4 +135,4 @@ int main( int argc, char *argv[] ){
   print_node(final);
 
   return 0;
-}*/
+}
